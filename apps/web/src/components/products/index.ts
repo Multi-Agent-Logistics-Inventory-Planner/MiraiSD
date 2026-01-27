a720cf9b14d8b08550cd23b69fa3306b0dbc4e28
Felipe Min <felipemin03@gmail.com>
2026-01-27 11:30:42 -0500

    refactor(products): improved product detail modal and improve type safety

--- apps/web/src/components/products/index.ts
+++ apps/web/src/components/products/index.ts
@@ -1,6 +1,13 @@
 export { ProductHeader } from "./product-header";
 export { ProductFilters, DEFAULT_PRODUCT_FILTERS } from "./product-filters";
-export type { ProductFiltersState, CategoryFilter, StatusFilter } from "./product-filters";
+export type {
+  ProductFiltersState,
+  CategoryFilter,
+  SubcategoryFilter,
+  StatusFilter,
+} from "./product-filters";
 export { ProductTable } from "./product-table";
 export { ProductPagination } from "./product-pagination";
-export { ProductDetailSheet } from "./product-detail-sheet";
+export { ProductModal } from "./product-modal";
+export { ProductForm } from "./product-form";
+export { DeleteProductDialog } from "./delete-product-dialog";
